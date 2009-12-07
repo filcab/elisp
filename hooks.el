11a1228bb0c827eb8f6f038e85f9369c91ec3d40
FilCab <filcab+git@gmail.com>
2009-12-07 22:22:19 +0000

    puts auto-compile in elisp. fix warnings

--- hooks.el
+++ hooks.el
@@ -1,40 +1,48 @@
 ;;;; -*- Mode: Emacs-Lisp -*-
 
-(add-hook 'emacs-lisp-mode-hook (lambda ()
-				  (setq indent-tabs-mode nil)))
+
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil)
+            (auto-compile-mode t)))
+
 
 ;;;; Lisp hooks:
 (add-hook 'lisp-mode-hook 'filcab-lisp-mode-hook)
 
-(add-hook 'slime-repl-mode-hook (lambda()
-				  (lisp-mode-more-brackets)))
+(add-hook 'slime-repl-mode-hook
+          (lambda ()
+            (lisp-mode-more-brackets)))
 
-(add-hook 'inferior-lisp-mode-hook (lambda ()
-				     (inferior-slime-mode t)))
+(add-hook 'inferior-lisp-mode-hook
+          (lambda ()
+            (inferior-slime-mode t)))
 
 
 ;;;; C-mode hooks:
-(add-hook 'c-mode-common-hook '(lambda ()
-				 (local-set-key (kbd "<return>") 'newline-and-indent)
-				 (c-set-style "gnu")
-				 (setq c-basic-offset 2)
-				 (setq c-indent-level 2)
-				 (setq c-tab-always-indent t)
-				 (setq tab-width 4)
-				 ;; indent com tabs
-				 (setq indent-tabs-mode nil)
-                                 (eldoc-mode))) ;; ElDoc
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (local-set-key (kbd "<return>") 'newline-and-indent)
+            (c-set-style "gnu")
+            (setq c-basic-offset 2)
+            (setq c-indent-level 2)
+            (setq c-tab-always-indent t)
+            (setq tab-width 4)
+            ;; indent com tabs
+            (setq indent-tabs-mode nil)
+            (eldoc-mode))) ;; ElDoc
 
 ;;;; Java hooks:
-(add-hook 'java-mode-common-hook '(lambda ()
-				    (c-set-style "k&r")
-				    (local-set-key (kbd "<return>") 'newline-and-indent)
-				    (setq c-basic-offset 4)
-				    (setq c-indent-level 4)
-				    (setq c-tab-always-indent nil)
-				    (setq tab-width 4)
-				    ;; indent com tabs
-				    (setq indent-tabs-mode nil)))
+(add-hook 'java-mode-common-hook
+          (lambda ()
+            (c-set-style "k&r")
+            (local-set-key (kbd "<return>") 'newline-and-indent)
+            (setq c-basic-offset 4)
+            (setq c-indent-level 4)
+            (setq c-tab-always-indent nil)
+            (setq tab-width 4)
+            ;; indent com tabs
+            (setq indent-tabs-mode nil)))
 
 ;;;; Haskell hooks
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
