e608bf8d8b2e19902e1d9b2c5149aa85203f84d1
FilCab <filcab+git@gmail.com>
2010-01-05 16:43:16 +0000

    correct emacs-daemon.sh... It works now for console emacs and X11 emacs

--- emacs-daemon.sh
+++ emacs-daemon.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-EMACS = `which emacs`
+EMACS="`which emacs`"
 
-if [ `uname` = "Darwin" ]
-then
-    EMACS = /Applications/Emacs.app/Contents/MacOS/Emacs
-fi
+# This isn't working due to the emacsclient side on the *.app
+#if [ "`uname`" = "Darwin" ]
+#then
+#    EMACS="/Applications/Emacs.app/Contents/MacOS/Emacs"
+#fi
 
 $EMACS --daemon
+
