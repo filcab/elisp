b82b70c828969b50cb55dd1628101ab7a169d510
FilCab <filcab+git@gmail.com>
2010-01-28 23:01:31 +0000

    Fix scheme-compile-file for a recent PLT-Scheme (change the sent string)

--- scheme-stuff.el
+++ scheme-stuff.el
@@ -8,7 +8,20 @@
 (defvar plt-dir "~/dev/stuff/plt/")
 (defvar quack-pltcollects-dir "~/dev/stuff/plt/collects/")
 
-
+;; Fix scheme-compile-file for a recent PLT-Scheme (change the sent string)
+(defun scheme-compile-file (file-name)
+  "Compile a Scheme file FILE-NAME in the inferior Scheme process."
+  (interactive (comint-get-source "Compile Scheme file: "
+                                  scheme-prev-l/c-dir/file
+                                  scheme-source-modes
+                                  nil)) ; nil because COMPILE doesn't
+                                        ; need an exact name.
+  (comint-check-source file-name) ; Check to see if buffer needs saved.
+  (setq scheme-prev-l/c-dir/file (cons (file-name-directory    file-name)
+                                       (file-name-nondirectory file-name)))
+  (comint-send-string (scheme-proc) (concat "(require (file \""
+                                            file-name
+                                            "\"\))\n")))
 
 ;(define-key scheme-mode-map [f1]
 ;  '(lambda ()
