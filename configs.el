7bde6e3d08132da78548f3b1c68f83a1c9c859a5
Filipe Cabecinhas <filcab@example.com>
2011-01-25 14:51:20 +0000

    configs: only auto-compile *.el files

--- configs.el
+++ configs.el
@@ -197,9 +197,10 @@
 
 ;; Emacs Lisp auto compile:
 (setq-default auto-compile-when nil)
-(setq-default auto-compile-flag 'compiledp-or-ask)
+;;(setq-default auto-compile-flag 'compiledp-or-ask)
 (setq-default auto-compile-remember nil)
 (setq auto-compile-exclude '("private.el.gpg$")) ; Don't compile private files
+(setq auto-compile-include '("\.el$"))
 (autoload 'auto-compile-mode "auto-compile" "Auto compile files" t)
 
 
